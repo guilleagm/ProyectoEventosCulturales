32b1303f6a73eb75bc49ed665c31fffe87d11030
guilleagm <guillermo.gonzalez.martinez.99@gmail.com>
2024-05-07 22:55:35 +0200

    Estilos login/register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,22 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/css/estilos1.css">
+    <link rel="stylesheet" href="{{ asset('css/estilosLogin.css') }}">
 </head>
 <body>
-<h1>Login</h1>
-<form method="POST" action="{{ route('login.submit') }}">
-    @csrf
-    <label for="correo">Correo:</label>
-    <input type="email" name="correo" required>
-    <br>
-    <label for="contraseña">Contraseña:</label>
-    <input type="password" name="contraseña" required>
-    <br>
-    <button type="submit">Login</button>
-</form>
+<div class="login-wrapper">
+    <div class="branding-section">
+    </div>
+
+    <div class="form-section">
+        <h1>Iniciar Sesión</h1>
+        <form method="POST" action="{{ route('login.submit') }}">
+            @csrf
+            <div class="form-group">
+                <label for="email">Correo</label>
+                <input type="email" id="email" name="correo" class="input-field" placeholder="Introduce tu correo electrónico" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input type="password" id="password" name="contraseña" class="input-field" placeholder="Contraseña" required>
+            </div>
+            <button type="submit" class="login-button">Iniciar sesión</button>
+            <div>
+                ¿No tienes cuenta aún?<a href="#">Regístrate</a>
+            </div>
+            <div>
+                Iniciar sesión con google
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,28 +2,38 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="/css/estilos1.css">
+    <link rel="stylesheet" href="{{ asset('css/estilosRegister.css') }}">
 </head>
 <body>
-<h1>Registro</h1>
-<form method="POST" action="{{ route('register.submit') }}" enctype="multipart/form-data">
-    @csrf
-    <label for="nombre_usuario">Nombre de usuario:</label>
-    <input type="text" name="nombre_usuario" id="nombre_usuario" required>
-    <br>
-    <label for="correo">Correo:</label>
-    <input type="email" name="correo" id="correo" required>
-    <br>
-    <label for="contraseña">Contraseña:</label>
-    <input type="password" name="contraseña" id="contraseña" required>
-    <br>
-    <label for="imagen">Imagen de perfil:</label>
-    <input type="file" name="imagen" id="imagen" accept="image/*">
-    <br>
-    <button type="submit">Registrar</button>
-</form>
+<div class="register-wrapper">
+    <div class="branding-section">
+    </div>
+
+    <div class="form-section">
+        <h1>Registro</h1>
+        <form method="POST" action="{{ route('register.submit') }}" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="nombre_usuario">Nombre de usuario</label>
+                <input type="text" name="nombre_usuario" id="nombre_usuario" class="input-field" placeholder="Introduce un nombre de usuario" required>
+            </div>
+            <div class="form-group">
+                <label for="correo">Correo</label>
+                <input type="email" name="correo" id="correo" class="input-field" placeholder="Introduce un correo" required>
+            </div>
+            <div class="form-group">
+                <label for="contraseña">Contraseña</label>
+                <input type="password" name="contraseña" id="contraseña" class="input-field" placeholder="Contraseña" required>
+            </div>
+            <div class="form-group">
+                <label for="imagen">Imagen de perfil</label>
+                <input type="file" name="imagen" id="imagen" accept="image/*" class="input-file">
+            </div>
+            <button type="submit" class="register-button">Registrar</button>
+        </form>
+    </div>
+</div>
 </body>
 </html>
